4e327aedbd432edf9c8816c38f326a1a477c63d9
Clyde Jackson <jacksonclyde818@gmail.com>
2025-03-17 13:17:11 -0400

    Initial Commit

---
+++ Project Journal/Project_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_JournalApp.swift
+//  Project Journal
+//
+//  Created by Clyde Jackson on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct Project_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Project JournalTests/Project_JournalTests.swift
@@ -0,0 +1,17 @@
+//
+//  Project_JournalTests.swift
+//  Project JournalTests
+//
+//  Created by Clyde Jackson on 3/17/25.
+//
+
+import Testing
+@testable import Project_Journal
+
+struct Project_JournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
